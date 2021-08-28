1d1606093cb307c17b28c9e13906693bab0908f7
Stinky-c <60587749+Stinky-c@users.noreply.github.com>
2021-08-28 12:10:37 -0700

    Update scavenge.js
    This is the first iteration, subject to change heavily

--- overrides/kubejs/server_scripts/Scavenge-concept/scavenge.js
+++ overrides/kubejs/server_scripts/Scavenge-concept/scavenge.js
@@ -1,5 +1,26 @@
 onEvent('recipes',event =>{
-
-
+    //cutting down on space
+    const {mixing,filling,splashing,sequenced_assembly,deploying,cutting}= event.recipes.create
+    //context:
+    //event.recipes.create.sequenced_assembly(output[], input, sequence[]).transitionalItem(item).loops(number)
+    sequenced_assembly(
+        [
+            item.of('emendatusenigmatica:copper_nugget'),
+            item.of('emendatusenigmatica:zinc_nugget'),
+            item.of('emendatusenigmatica:silver_nugget'),
+            item.of('emendatusenigmatica:lead_nugget'),
+            item.of('minecraft:iron_nugget'),
+            item.of('emendatusenigmatica:copper_nugget',3),
+            item.of('emendatusenigmatica:zinc_nugget',3),
+            item.of('emendatusenigmatica:silver_nugget',3),
+            item.of('emendatusenigmatica:lead_nugget',3),
+            item.of('minecraft:iron_nugget',3),
+        ],
+            item.of('minecraft:gravel'),
+        [
+            filling('minecraft:gravel',['minecraft:gravel',fluid.of('minecraft:water',200)]),
+            cutting('minecraft:gravel','minecraft:gravel').processingTime(100)
+        ]
+        ).transitionalItem('minecraft:gravel').id('kjs_scavenge:gravel').loops(2)
 
 })
