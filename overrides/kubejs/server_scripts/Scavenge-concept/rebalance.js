8d8288710e154409b5111bd41d6db617767d5e83
Stinky-c <60587749+Stinky-c@users.noreply.github.com>
2021-08-30 10:10:52 -0700

    Create rebalance.js

---
+++ overrides/kubejs/server_scripts/Scavenge-concept/rebalance.js
@@ -0,0 +1,21 @@
+onEvent('recipes',event =>{
+    const {compacting,filling} = event.recipes.create
+    //manyullyn ingot
+    event.remove({id:'tconstruct:smeltery/melting/metal/manyullyn/nugget'})
+    event.remove({id:'tconstruct:common/materials/manyullyn_ingot_from_nuggets'})
+    compacting('tconstruct:manyullyn_ingot',item.of('tconstruct:manyullyn_nugget',9)).superheated().id('kjs_scavenge:pressing/manyullyn_ingot')
+
+    //debris
+    event.remove({id:'tconstruct:smeltery/melting/metal/molten_debris/debris_nugget'})
+    compacting('mekanism:dirty_netherite_scrap',item.of('tconstruct:debris_nugget',9)).superheated().id('kjs_scavenge:pressing/dirty_netherite_scrap')
+    
+    //blaze cake
+    event.remove({id:'create:filling/blaze_cake'})
+    Filling(
+            'create:blaze_cake', 
+        [
+            'create:blaze_cake_base',
+            fluid.of('tconstruct:blazing_blood', 250)
+        ]
+    ).id('kjs_scavenge:filling/blaze_cake')
+})
