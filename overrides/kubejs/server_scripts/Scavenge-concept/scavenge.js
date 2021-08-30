b09a7de12963381c3e37090c9c6dcbf140e5f81b
Stinky-c <60587749+Stinky-c@users.noreply.github.com>
2021-08-29 21:10:48 -0700

    Update scavenge.js
    added some drops,  chances are soon to come

--- overrides/kubejs/server_scripts/Scavenge-concept/scavenge.js
+++ overrides/kubejs/server_scripts/Scavenge-concept/scavenge.js
@@ -3,6 +3,8 @@ onEvent('recipes',event =>{
     const {mixing,filling,splashing,sequenced_assembly,deploying,cutting}= event.recipes.create
     //context:
     //event.recipes.create.sequenced_assembly(output[], input, sequence[]).transitionalItem(item).loops(number)
+
+    //gravel
     sequenced_assembly(
         [
             item.of('emendatusenigmatica:copper_nugget'),
@@ -22,5 +24,68 @@ onEvent('recipes',event =>{
             cutting('minecraft:gravel','minecraft:gravel').processingTime(100)
         ]
         ).transitionalItem('minecraft:gravel').id('kjs_scavenge:gravel').loops(2)
+    
+    //limesand
+    sequenced_assembly(
+        [
+            item.of('minecraft:lapis_lazuli'),
+            item.of('minecraft:clay_ball'),
+            item.of('minecraft:bone_meal'),
+            item.of('emendatusenigmatica:silicon_gem'),
+            item.of('minecraft:redstone'),
 
+            item.of('minecraft:lapis_lazuli',3),
+            item.of('minecraft:clay_ball',3),
+            item.of('minecraft:bone_meal',3),
+            item.of('emendatusenigmatica:silicon_gem',3),
+            item.of('minecraft:redstone',3)
+        ],
+            item.of('create:limesand'),
+        [
+            filling('minecraft:gravel',['minecraft:gravel',fluid.of('minecraft:water',200)]),
+            cutting('minecraft:gravel','minecraft:gravel').processingTime(100)
+        ]
+    ).transitionalItem('create:limesand').id('kjs_scavenge:limesand').loops(2)
+
+    //sand
+    sequenced_assembly(
+        [
+            item.of('minecraft:gold_nugget'),
+            item.of('emendatusenigmatica:tin_nugget'),
+            item.of('minecraft:clay_ball'),
+            item.of('botania:quartz_sunny'),
+
+            item.of('minecraft:gold_nugget',3),
+            item.of('emendatusenigmatica:tin_nugget',3),
+            item.of('minecraft:clay_ball',3),
+            item.of('botania:quartz_sunny',3),
+            item.of('tconstruct:quartz_shuriken',2)
+        ],
+        item.of('minecraft:sand'),
+        [
+            filling('minecraft:gravel',['minecraft:gravel',fluid.of('minecraft:water',200)]),
+            cutting('minecraft:gravel','minecraft:gravel').processingTime(100)
+        ]
+    ).transitionalItem('minecraft:sand').id('kjs_scavenge:sand').loops(2)
+
+    //netherrack
+    sequenced_assembly(
+        [
+            item.of('quark:soul_bead'),
+            item.of('create:rose_quartz'),
+            item.of('tconstruct:scorched_brick'),
+
+            item.of('tconstruct:ichor_slime_ball'),
+            item.of('create:cinder_flour',3),
+            item.of('tconstruct:cobalt_nugget',2),
+
+            item.of('tconstruct:manyullyn_nugget'),
+            item.of('tconstruct:debris_nugget'),
+        ],
+            item.of('minecraft:netherrack'),
+        [
+            filling('minecraft:gravel',['minecraft:gravel',fluid.of('minecraft:water',200)]),
+            cutting('minecraft:gravel','minecraft:gravel').processingTime(100)
+        ]
+    ).transitionalItem('minecraft:netherrack').id('kjs_scavenge:netherrack').loops(2)
 })
