fea7f29e01029f978580a52bada18ae59c168e2d
Talan122 <63525338+Talan122@users.noreply.github.com>
2021-10-26 19:41:40 -0400

    Remove buildtools and workflows

--- buildtools/buildConfig.ts
+++
@@ -1,8 +0,0 @@
-import buildConfig from "./util/buildConfig.default.json";
-import fs from "fs";
-
-if (fs.existsSync("./config.json")) {
-	Object.assign(buildConfig, JSON.parse(fs.readFileSync("./config.json").toString()));
-}
-
-export default buildConfig;
--- buildtools/gulpfile.ts
+++
@@ -1,4 +0,0 @@
-import * as gulp from "gulp";
-
-import * as gha from "./tasks/misc/gha";
-export const makeArtifactNames = gha.makeArtifactNames;
